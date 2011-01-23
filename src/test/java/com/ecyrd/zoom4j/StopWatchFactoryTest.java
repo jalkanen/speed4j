6fe9a7d734ce5e93740d5476302725a3a2f9fb28
Janne Jalkanen <jalkanen@ecyrd.com>
2011-01-23 16:39:13 +0200

    Removed obsolete imports; added copyright header.

--- src/test/java/com/ecyrd/zoom4j/StopWatchFactoryTest.java
+++ src/test/java/com/ecyrd/zoom4j/StopWatchFactoryTest.java
@@ -15,9 +15,6 @@
 */
 package com.ecyrd.zoom4j;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import com.ecyrd.zoom4j.log.PeriodicalLog;
