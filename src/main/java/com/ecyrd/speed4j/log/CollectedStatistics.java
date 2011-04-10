22e86cbc2eaf3b348dae813358c857d34a192b82
Janne Jalkanen <jalkanen@ecyrd.com>
2011-04-10 11:12:16 +0300

    Improved javadocs.

--- src/main/java/com/ecyrd/speed4j/log/CollectedStatistics.java
+++ src/main/java/com/ecyrd/speed4j/log/CollectedStatistics.java
@@ -20,7 +20,16 @@ import java.util.List;
 
 import com.ecyrd.speed4j.StopWatch;
 
-class CollectedStatistics
+/**
+ *  Represents a statistics item which contains data which has been collected
+ *  over time.  It keeps track of maximum and minimum times, as well as a count,
+ *  average and standard deviation.
+ *  <p>
+ *  Internally, it keeps the nanosecond accuracy, though most JVMs and operating
+ *  systems can usually go down to microseconds only.  However, all API calls
+ *  return milliseconds (as doubles), as it's the most useful figure.
+ */
+public class CollectedStatistics
 {
     private List<Double> m_times = new ArrayList<Double>();
     private double m_min = Double.MAX_VALUE;
@@ -28,6 +37,11 @@ class CollectedStatistics
     
     private double NANOS_IN_MILLIS = 1e6;
     
+    /**
+     *  Add a StopWatch to the statistics.
+     *  
+     *  @param sw StopWatch to add.
+     */
     public void add(StopWatch sw)
     {
         double timeInMs = sw.getTimeNanos() / NANOS_IN_MILLIS;
@@ -37,21 +51,44 @@ class CollectedStatistics
         if( timeInMs > m_max ) m_max = timeInMs;
     }
     
+    /**
+     *  Returns the number of stopwatches.  This is a fast operation.
+     *  
+     *  @return Number of StopWatches.
+     */
     public int getInvocations()
     {
         return m_times.size();
     }
     
+    /**
+     *  Returns the fastest StopWatch time recorded.  This is a fast
+     *  operation.
+     *  
+     *  @return Fastest in milliseconds.
+     */
     public double getMin()
     {
         return m_min;
     }
     
+    /**
+     *  Returns the slowest StopWatch time recorded.  This is a fast
+     *  operation.
+     *  
+     *  @return Slowest in milliseconds.
+     */
     public double getMax()
     {
         return m_max;
     }
     
+    /**
+     *  Returns the average of the StopWatches recorded.  NB: This call
+     *  causes all of the StopWatches to be traversed, which makes it fairly slow.
+     *  
+     *  @return The average in milliseconds.
+     */
     public double getAverageMS()
     {
         double result = 0.0;
@@ -64,11 +101,23 @@ class CollectedStatistics
         return result / m_times.size();
     }
     
+    /**
+     *  Returns the standard deviation of all StopWatches recorded.  NB: This
+     *  call causes all of the StopWatches to be traversed, which makes it fairly slow.
+     *  
+     *  @return The standard deviation.
+     */
     public double getStdDev()
     {
         return Math.sqrt(variance());
     }
     
+    /**
+     *  Returns the variance of all StopWatches recorded. NB: This
+     *  call causes all of the StopWatches to be traversed, which makes it fairly slow.
+     *  
+     *  @return The variance.
+     */
     public double variance() 
     {
         long n = 0;
